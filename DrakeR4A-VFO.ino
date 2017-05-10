50f68c2497ed5188cccc90833008249ae089fffa
ph2lb <lex@ph2lb.nl>
2017-05-10 15:19:18 +0200

    changed rotary encode routine for simple encoder.

--- DrakeR4A-VFO.ino
+++ DrakeR4A-VFO.ino
@@ -210,17 +210,20 @@ void setup()
 
   //call updateEncoder() when any high/low changed seen
   //on interrupt 0 (pin 2), or interrupt 1 (pin 3) 
-  attachInterrupt(0, updateEncoder, CHANGE); 
-  //attachInterrupt(1, updateEncoder, CHANGE);
+  attachInterrupt(0, updateEncoder, FALLING);  
   setFreq();
 } 
 
 
 void updateEncoder()
 {
+
+  // a falling edge on encoderPin1 will call this routine.
+  // keep it short and simple. 
+#ifdef TRUEGRAYENCODER
+  // when having a true gray encoder (one click = one bit change)
   int MSB = digitalRead(encoderPin1); //MSB = most significant bit
   int LSB = digitalRead(encoderPin2); //LSB = least significant bit
-
   int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
   int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
 
@@ -228,6 +231,15 @@ void updateEncoder()
   if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
 
   lastEncoded = encoded; //store this value for next time
+#else
+  // when using a cheap gray encoder (one click = a full train pulses)
+  int encoderPin2State = digitalRead(encoderPin2); //LSB = least significant bit
+  if (encoderPin2State == 1)  
+    encoderValue ++;
+  else
+    encoderValue --;
+#endif
+
 }
 
 
